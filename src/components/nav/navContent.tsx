c8c3ebf546fc8d4d2989a7dc24f6067c7d906e7f
Hammad Afzal <127996183+hammad42662@users.noreply.github.com>
2024-08-18 19:37:04 +0100

    Update navContent.tsx

--- src/components/nav/navContent.tsx
+++ src/components/nav/navContent.tsx
@@ -4,13 +4,13 @@ export default function NavContent() {
   return (
     <div className="flex-row justify-center items-center">
       <div className=" flex flex-row justify-center items-center gap-2">
-        <span className=" border-x-2 border-y-2 px-2 py-2 bg-green-200 border-green-400">
-          <GiTimeTrap size="3rem" color="gray" />
+        <span className=" border-x-2 border-y-2 px-2 py-2 ">
+          <GiTimeTrap size="3rem" color="black" />
         </span>
-        <h1 className="text-3xl font-bold text-green-700">Task Box</h1>
+        <h1 className="text-3xl font-bold text-zinc-800">Task Box</h1>
       </div>
       <div className="flex justify-center items-center mt-4">
-        <h2 className="text-xl">Your Daily Schedule Planner</h2>
+        <h2 className="text-xl text-zinc-800">Your Daily Schedule Planner</h2>
       </div>
     </div>
   );
