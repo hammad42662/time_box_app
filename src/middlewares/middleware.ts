eaf5a40afa1b4dd200e13f66bd4db9cc07f978e2
Hammad Afzal <127996183+hammad42662@users.noreply.github.com>
2024-08-19 01:27:00 +0100

    Update middleware.ts

--- src/middlewares/middleware.ts
+++ src/middlewares/middleware.ts
@@ -2,7 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET =
-  process.env.JWT_SECRET 
+  process.env.JWT_SECRET ||
+  "19640645bdac4d47e6f359079617dc0ccfd024cfa2cc4b1a170b7be4a97b7b58";
 
 export function middleware(request: NextRequest) {
   const token = request.headers.get("Authorization")?.replace("Bearer ", "");
