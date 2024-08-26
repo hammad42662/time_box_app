1c06a723aa5a60771ba1f11e4d03988a6df5293f
Hammad Afzal <hammadafzal05@gmail.com>
2024-08-26 22:53:18 +0100

    CTA style

--- src/components/landing/CallToAction.tsx
+++ src/components/landing/CallToAction.tsx
@@ -5,7 +5,7 @@ export default function CallToAction() {
       <div className="container flex flex-col items-center justify-between px-6 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
         {/* Heading */}
         <h2 className="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left">
-          Simplify how your team works today
+          Make your day super productive
         </h2>
         {/* Button */}
         <div>
