c4924db47d88af0ca5ba7d57eae4446ee8b7b18b
Hammad Afzal <hammadafzal05@gmail.com>
2024-09-05 02:48:16 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ClientProvider from "./redux/provider";
+import Script from "next/script";
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LBHT2K3VW0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-LBHT2K3VW0');`}
+        </Script>
+      </head>
       <body className={`${poppins.className} `}>
         <ClientProvider>{children}</ClientProvider>
       </body>
